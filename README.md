b51c3da06f9a9ab4731d2361708fed3c929e1722
LucaRosola <lucarosola@gmail.com>
2021-03-13 10:30:27 +0100

    Add a configuration about errors

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Prenotazioni
 
-## Esempio didattico per la gestione delle prenotazioni dei tamponi
+## Esempio didattico per gestione delle prenotazioni dei tamponi
 
-Durante l'epidemia di COVID, una delle esigenze è stata quella di gestire la prenotazione di tamponi.
-Partendo da questo spunto verrà sviluppata un'applicazione che risolva questo problema.
+Durante l'epidemia di COVID, una delle esigenze è stata quella di gestire la prenotazione dei tamponi.
+Partendo da questo spunto, verrà qui sviluppata un'applicazione che risolva questo problema.
 
 ### Funzionalità
-- Inserire una prenotazione con codice fiscale e data
+
+- Inserire una **prenotazione** con codice fiscale e data
--- prenota.php
+++ prenota.php
@@ -1,4 +1,28 @@
 <?php
 
-ini_set("display_errors", 1);
-ini_set('log_errors', 0);
+include_once "config.php";
+
+//Variabili valorizzate tramite POST
+$codice_fiscale = $_POST['codice'];
+$giorno = $_POST['giorno'];
+
+//Query di inserimento preparata
+$sql = "INSERT INTO tampone.prenotazioni VALUES(null, :codice_fiscale, :giorno)";
+
+//Inviamo la query al database che la tiene in pancia
+$stmt = $pdo->prepare($sql);
+
+//Inviamo i dati conreti che verranno messi al posto dei segnaposto
+$stmt->execute(
+    [
+        'codice_fiscale' => $codice_fiscale,
+        'giorno' => $giorno
+    ]
+);
+
+//Ridirige il browser verso la pagina indicata nella location
+//Serve come modo diretto per vedere attraverso il browser che la pagina
+//ha effettivamente prodotto un risultato
+header('Location:lista_prenotazioni.php');
+
+
